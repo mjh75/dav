8de67b7d6348ae51733532c364e147831e60e83a
Michael J. Hartwick <hartwick@hartwick.com>
2016-03-15 17:06:42 -0400

    * Stop mixing languages

--- DAVAdmin/create.php
+++ DAVAdmin/create.php
@@ -4,7 +4,7 @@
 	$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
 	$password = filter_input(INPUT_POST, 'password');
 	
-	$users = "INSERT INTO `users` SET `username` = :username, `password` = hash('sha256', :password)";
+	$users = "INSERT INTO `users` SET `username` = :username, `password` = sha2(:password, 256)";
 	$prin1 = "INSERT INTO `principals` SET `uri` = CONCAT('principals/', :username), `email`=:email, `displayname`=:displayname";
 	$prin2 = "INSERT INTO `principals` SET `uri` = CONCAT('principals/', :username, '/calendar-proxy-read')";
 	$prin3 = "INSERT INTO `principals` SET `uri` = CONCAT('principals/', :username, '/calendar-proxy-write')";
