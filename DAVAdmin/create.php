a71350975c007c3b43d013c4c89fe688c538634c
Michael J. Hartwick <hartwick@hartwick.com>
2016-03-15 17:02:49 -0400

    * When preparing statements don't quote the variable

--- DAVAdmin/create.php
+++ DAVAdmin/create.php
@@ -4,10 +4,10 @@
 	$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
 	$password = filter_input(INPUT_POST, 'password');
 	
-	$users = "INSERT INTO `users` SET `username` = ':username', `password` = hash('sha256', ':password')";
-	$prin1 = "INSERT INTO `principals` SET `uri` = 'principals/:username', `email`=':email', `displayname`=':displayname'";
-	$prin2 = "INSERT INTO `principals` SET `uri` = 'principals/:username/calendar-proxy-read'";
-	$prin3 = "INSERT INTO `principals` SET `uri` = 'principals/:username/calendar-proxy-write'";
+	$users = "INSERT INTO `users` SET `username` = :username, `password` = hash('sha256', :password)";
+	$prin1 = "INSERT INTO `principals` SET `uri` = CONCAT('principals/', :username), `email`=:email, `displayname`=:displayname";
+	$prin2 = "INSERT INTO `principals` SET `uri` = CONCAT('principals/', :username, '/calendar-proxy-read')";
+	$prin3 = "INSERT INTO `principals` SET `uri` = CONCAT('principals/', :username, '/calendar-proxy-write')";
 
 	try {
 		$dbh = new PDO("mysql:host=localhost;dbname=dav", "dav", "plantronics");
