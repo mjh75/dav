75a48b21c2c22c267c4df6b35060a4564861e2ac
Michael J. Hartwick <hartwick@hartwick.com>
2016-03-15 16:47:07 -0400

    * Apparently INPUT_REQUEST is not yet implemented

--- DAVAdmin/create.php
+++ DAVAdmin/create.php
@@ -1,8 +1,8 @@
 <?php
-	$username = filter_input(INPUT_REQUEST, 'username');
-	$displayname = filter_input(INPUT_REQUEST, 'displayname');
-	$email = filter_input(INPUT_REQUEST, 'email', FILTER_VALIDATE_EMAIL);
-	$password = filter_input(INPUT_REQUEST, 'password');
+	$username = filter_input(INPUT_POST, 'username');
+	$displayname = filter_input(INPUT_POST, 'displayname');
+	$email = filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL);
+	$password = filter_input(INPUT_POST, 'password');
 	
 	$users = "INSERT INTO `users` SET `username` = '$username', `password` = hash('sha256', $password)";
 	$prin1 = "INSERT INTO `principals` SET `uri` = 'principals/$username', `email`='$email', `displayname`='$displayname'";
